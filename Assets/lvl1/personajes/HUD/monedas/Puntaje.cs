fd41062bdac9645d058e92e598e0e65c1b5d1cff
MariJ20-Shipper <mual80@hotmail.com>
2019-10-23 11:34:01 -0500

    coño

---
+++ Assets/lvl1/personajes/HUD/monedas/Puntaje.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puntaje : MonoBehaviour
+{
+    public GameObject decenas, unidades;
+    public PlayerController heroe;
+    private string[] estados = { "Estado_00", "Estado_01", "Estado_02", "Estado_03", "Estado_04", "Estado_05", "Estado_06", "Estado_07", "Estado_08", "Estado_09" };
+    private Animator de;
+    private Animator un;
+
+    void Start ()
+    { 
+    de=decenas.GetComponent<Animator>();
+    un = unidades.GetComponent<Animator>();
+
+    ActualizarContador(heroe.Puntaje);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void ActualizarContador(int numero)
+    {
+
+        int unidades = numero % 10;
+        int decenas = numero % 100 - unidades;
+        Debug.Log("Decenas" + decenas / 10 + "Unidades" + unidades);
+        decenas = decenas / 10;
+
+        if (numero > 9)
+        {
+            //Hay Decenas
+            de.Play(estados[decenas]);
+        }
+        else
+        {
+            de.Play(estados[0]);
+
+        }
+        un.Play(estados[unidades]);
+    }
+}
+
+
---
+++ Assets/scripts/PauseScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject PausaFinalUI;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (GameIsPaused)
+            {
+                Continuar();
+
+            }
+            else
+            {
+                Pausa();
+            }
+        }
+    }
+    public void Continuar()
+    {
+        PausaFinalUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    void Pausa()
+    {
+        PausaFinalUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
